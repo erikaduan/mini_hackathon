c9fada461fa49e5b3a9f44b5f76a42d3aa76faf2
Sean Malcolm <anon@gmail.com>
2022-07-28 13:50:27 +1000

    started geographic integration

---
+++ geographic_integration.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+
+
+
+
+
+
+
+def main():
+
+
+	df = pd.read_csv("Traffic_Camera_Locations.csv")
+
+
+
+
+if __name__ == '__main__':
+	main()
--- sean_script.R
+++ sean_script.R
@@ -2,10 +2,11 @@ library(dplyr)
 library(ggplot2)
 
 df <- read.csv("Traffic_camera_offences_and_fines.csv")
+codes <- read.csv("Traffic_Camera_Locations.csv")
 
 
 df <- filter(df, Sum_Pen_Amt <= 50000)
 
 p <- ggplot(data = df, aes(x=Sum_Pen_Amt)) +
-  geom_hist() +
+  geom_histogram(binwidth = 5) +
   scale_x_log10()
