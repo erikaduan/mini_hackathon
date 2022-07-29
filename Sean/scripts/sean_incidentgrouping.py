85afd7008f98c8bd6802c794f5915969adf7fa0a
Sean Malcolm <anon@gmail.com>
2022-07-29 13:00:36 +1000

    Fixed scripts to work with new folder structure Added html output for map
    Signed-off-by: Sean Malcolm <anon@gmail.com>

---
+++ Sean/scripts/sean_incidentgrouping.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os.path
+
+
+if __name__ == '__main__':
+    
+
+    
+    
+    df = pd.read_csv(os.path.dirname(__file__) + "/../data/traffic_camera_offences_geographic.csv")
+    df = df.dropna()
+
+    camera_counts = {}
+    
+    #Create a dictionary with the location codes as keys
+    for index, row in df.iterrows():
+        
+        code = row['Location_Code']
+        if code in camera_counts:
+            camera_counts[code]['total_pen'] += row['Sum_Pen_Amt']
+            camera_counts[code]['total_incidents'] += 1
+        else:
+            camera_counts[code] = {}
+            camera_counts[code]['total_pen'] = row['Sum_Pen_Amt']
+            camera_counts[code]['total_incidents'] = 1
+   
+            camera_counts[code]['Camera_Type'] = row['Camera_Type']
+            camera_counts[code]['Location_Desc'] = row['Location_Desc'] 
+            camera_counts[code]['lat'] = row['lat']
+            camera_counts[code]['lon'] = row['lon'] 
+    
+    #Create dataset with each camera as one row
+    df_cameras = pd.DataFrame.from_dict(camera_counts, orient='index')
+    
+    df_cameras.to_csv(os.path.dirname(__file__) + "/../data/camera_data.csv")
+            
+    
